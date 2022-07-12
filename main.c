d9914b99e6149465907d78fbb15efbc69375f482
Zoolmabool <yan.baudouin@gmail.com>
2022-07-12 15:36:31 +0200

    ajout  p_a et p_b

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ybaudoui <ybaudoui@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:31:31 by ybaudoui          #+#    #+#             */
-/*   Updated: 2022/07/08 18:18:53 by ybaudoui         ###   ########.fr       */
+/*   Updated: 2022/07/12 15:32:41 by ybaudoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,28 +103,62 @@ void    ft_reverse_rotate(t_pile *pile)
     pile->top->content = nbr;
 }
 
+void    ft_pb(t_pile *pile_a, t_pile *pile_b)
+{ 
+    t_elements  *tmp;
+
+    if (!pile_a->top)
+        return ;
+    tmp = pile_a->top->next;
+    ft_addfront(pile_b, pile_a->top);
+    pile_a->top = tmp;
+}
+
+void    ft_pa(t_pile *pile_a, t_pile *pile_b)
+{
+    t_elements *tmp;
+
+    if (!pile_b->top)
+        return ;
+    tmp = pile_b->top->next;
+    ft_addfront(pile_a, pile_b->top);
+    pile_b->top = tmp;
+}
+
 int main(int argc, char **argv)
 {
-    t_pile  pile_a;
-    t_pile  pile_b;
+    t_pile      pile_a;
+    t_pile      pile_b;
     t_elements  *new;
 
     if (argc < 2)
         return (1);
     pile_a.top = NULL;
+    pile_b.top = NULL;
     new = ft_create(ft_atoi(argv[1]));
     ft_addfront(&pile_a, new);
+  //  free(new);
     new = ft_create(ft_atoi(argv[2]));
     ft_addback(&pile_a, new);
+    new = ft_create(ft_atoi(argv[3]));
+    ft_addback(&pile_a, new);
+    printf("%d\n%d\n%d\n\n", pile_a.top->content, pile_a.top->next->content, pile_a.top->next->next->content);
+    ft_pb(&pile_a, &pile_b);
+    ft_pb(&pile_a, &pile_b);
   //  printf("%d\n%d\n", pile_a.top->content, pile_a.top->next->content);
  //   ft_swap(&pile_a);
  //   printf("%d\n%d\n", pile_a.top->content, pile_a.top->next->content);
-    new = ft_create(ft_atoi(argv[3]));
-    ft_addback(&pile_a, new);
+   // new = ft_create(ft_atoi(argv[3]));
+  //  ft_addback(&pile_a, new);
  //   printf("%d\n%d\n%d\n\n", pile_a.top->content, pile_a.top->next->content, pile_a.top->next->next->content);
  //   ft_rotate(&pile_a);
  //   printf("%d\n%d\n%d\n\n", pile_a.top->content, pile_a.top->next->content, pile_a.top->next->next->content);
-    printf("%d\n%d\n", pile_a.top->content, pile_a.top->next->content);
-    ft_reverse_rotate(&pile_a);
-    printf("%d\n%d\n", pile_a.top->content, pile_a.top->next->content);
+    printf("%d\n\n", pile_a.top->content);
+    printf("%d\n%d\n\n", pile_b.top->content, pile_b.top->next->content);
+    ft_pa(&pile_a, &pile_b);
+    printf("%d\n%d\n\n", pile_a.top->content, pile_a.top->next->content);
+    printf("%d\n", pile_b.top->content);
+   // ft_reverse_rotate(&pile_a);
+   // printf("%d\n%d\n", pile_a.top->content, pile_a.top->next->content);
+  // free(new);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,17 +6,17 @@
 /*   By: ybaudoui <ybaudoui@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:31:26 by ybaudoui          #+#    #+#             */
-/*   Updated: 2022/07/08 16:37:17 by ybaudoui         ###   ########.fr       */
+/*   Updated: 2022/07/12 11:00:18 by ybaudoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "libft.h"
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "libft.h"
 
 typedef struct s_elements
 {
